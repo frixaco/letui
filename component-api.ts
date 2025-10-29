b1d38be211dd92c7905e91ba2e4b9d9a8ba0d66e
frixaco <rustam21ashurmatov@gmail.com>
2025-10-29 14:58:45 +0500

    add primitive for async; cleanup

--- component-api.ts
+++ component-api.ts
@@ -22,7 +22,7 @@ function schedule(fn: () => void) {
   });
 }
 
-function $<T>(defaultValue: T) {
+export function $<T>(defaultValue: T) {
   let v: T = defaultValue;
   let subs = new Set<() => void>();
 
@@ -31,7 +31,7 @@ function $<T>(defaultValue: T) {
       if (caller) subs.add(caller);
       return v;
     },
-    set(newValue: any) {
+    set(newValue: T) {
       v = newValue;
 
       for (let s of subs) schedule(s);
@@ -39,7 +39,7 @@ function $<T>(defaultValue: T) {
   };
 }
 
-function dd<T>(fn: () => T) {
+export function dd<T>(fn: () => T) {
   let v: T;
   let subs = new Set<() => void>();
 
@@ -72,7 +72,7 @@ function dd<T>(fn: () => T) {
   };
 }
 
-function ff(fn: () => void) {
+export function ff(fn: () => void) {
   let prev = caller;
   try {
     caller = fn;
@@ -82,16 +82,94 @@ function ff(fn: () => void) {
   }
 }
 
-const counter = $(0);
-const doubleCounter = dd(() => counter.get() * 2);
+let currentCtrl: AbortController | null = null;
 
-ff(() => {
-  console.log("fc", counter.get() + 1); // prints 1 first time, 2 second time
-});
+export function af<T, S = undefined>(
+  srcOrFn: S extends undefined ? () => Promise<T | null> : () => S,
+  fn?: (src: S) => Promise<T | null>,
+) {
+  let data = $<T | null>(null);
+  let loading = $(false);
 
-counter.set(1);
-await Promise.resolve();
-console.log("dc", doubleCounter.get()); // prints 2 cause of line above
+  async function fetchData(src?: S): Promise<undefined> {
+    let ctrl = new AbortController();
+    currentCtrl?.abort();
+    currentCtrl = ctrl;
+
+    loading.set(true);
+
+    try {
+      let v: T | null = null;
+      if (fn && src !== undefined) {
+        v = await fn(src);
+      } else {
+        v = await (srcOrFn as () => Promise<T | null>)();
+      }
+
+      if (ctrl.signal.aborted) return;
+
+      data.set(v);
+    } finally {
+      loading.set(false);
+    }
+  }
+
+  ff(() => {
+    if (!fn) {
+      fetchData();
+    } else {
+      const v = srcOrFn() as S;
+      fetchData(v);
+    }
+  });
+
+  return {
+    data,
+    loading,
+  };
+}
+
+// const counter = $(0);
+
+// const { data, loading } = af(
+//   () => counter.get(),
+//   async (c) => {
+//     console.log("async effect");
+//     await wait();
+//     return c + 100;
+//   },
+// );
+
+// ff(() => {
+//   console.log("==========");
+//   console.log(data.get());
+//   console.log(loading.get());
+//   console.log("==========");
+// });
+//
+// counter.set(1);
+// await Promise.resolve();
+// await wait(600);
+// console.log("first fetch done");
+//
+// counter.set(2);
+// await Promise.resolve();
+// await wait(600);
+// console.log("second fetch done");
+//
+// function wait(ms: number = 1000) {
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// }
+
+// const doubleCounter = dd(() => counter.get() * 2);
+//
+// ff(() => {
+//   console.log("fc", counter.get() + 1); // prints 1 first time, 2 second time
+// });
+//
+// counter.set(1);
+// await Promise.resolve();
+// console.log("dc", doubleCounter.get()); // prints 2 cause of line above
 
 // Output:
 // ❯ bun run component-api.ts
