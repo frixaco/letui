59ac889d39e3e74e4e0a3c6e7eff264f7a54ccea
frixaco <rustam21ashurmatov@gmail.com>
2026-02-12 02:35:38 +0500

    fix crashing caused by logger

--- src/debug.ts
+++ src/debug.ts
@@ -1,5 +1,15 @@
-const logFile = Bun.file("dump/logs.txt");
-export const logWriter = logFile.writer();
+function createLogWriter() {
+  try {
+    return Bun.file("dump/logs.txt").writer();
+  } catch {
+    return {
+      write(..._args: unknown[]) {},
+      flush(..._args: unknown[]) {},
+    };
+  }
+}
+
+export const logWriter = createLogWriter();
 
 export function log(txt: string, ...args: string[]) {
   logWriter.write(txt + " " + args.join(" ") + "\n");
